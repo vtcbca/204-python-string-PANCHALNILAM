f2bd6c49eb66b80368fe0c09b06e19cdca1cd766
PANCHALNILAM <100265108+PANCHALNILAM@users.noreply.github.com>
2022-03-16 13:51:32 +0530

    Update Prog16.py.py

--- Prog16.py.py
+++ Prog16.py.py
@@ -1,8 +1,12 @@
-a =input("Enter the string:")
-if(a[::-1]):
-   print("The string is  palindrom .")
+a =input("Enter any word for checking palindrome or not:")
+b=a[::-1]
+print(b)
+if(a==b):
+   print("string is palindrome")
+   print("{} is palindrome."format(a))
 else:
-    print("The string is  Not a palindrom.")
+    print("string is not palindrome")
+    print("{} is not palindrome."format(a))
     
 
     
